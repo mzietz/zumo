5d2863a82d4d5e32d99922fa034f286afeceef52
mzietz <zietzmaximilian@gmail.com>
2018-10-21 18:25:26 +0200

    first commit

---
+++ scripts/zumo.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+
+import serial
+from math import sqrt
+import rospy
+from time import sleep
+from threading import Lock
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Imu
+
+class Zumo:
+	"""docstring for Zumo"""
+	def __init__(self):
+		self.pub_imu=rospy.Publisher("/imu", Imu, queue_size=10)
+		try:
+			self.PORT=rospy.get_param('ZUMO_PORT')
+		except:
+			rospy.set_param('ZUMO_PORT',"dev/ttyACM0")
+			self.PORT=rospy.get_param('ZUMO_PORT')
+		try:
+			self.BAUDRATE=rospy.get_param('ZUMO_BAUDRATE') 
+		except:
+			rospy.set_param('ZUMO_BAUDRATE',"9600")
+			self.BAUDRATE=rospy.get_param('ZUMO_BAUDRATE')
+		self.TIMEOUT=0.1
+		self.lock=Lock()
+		self.message = list()
+		self.p=Imu()
+		self.p.header.stamp = rospy.Time.now()
+		self.p.header.frame_id="map"
+		try:
+			self.ser = serial.Serial( self.PORT, self.BAUDRATE,timeout=self.TIMEOUT)
+			sleep(1)
+			rospy.loginfo("Connection established at "+str(self.PORT))
+
+		except:
+			rospy.loginfo("Try to connect")
+			
+	def __delete__(self):
+		self.ser.close()
+		print "Connection lost"
+		
+	def get_message(self):
+		with self.lock: 
+		#si je rentre dedans je prends le lock et personne ne peut le prendre permet de gerer 
+		#les conflits de com sur le port serie, un seul dessus a la fois
+			try:
+				self.ser.flush()
+				sleep(0.001)
+				line = self.ser.readline() # reception trame accelero/magneto/gyro
+				if len(line)>0:
+					if line.startswith('!AN'):
+						line = line.replace("!AN:", "")
+						line = line.replace("\r\n", "")
+						self.message=line.split(',')
+						rospy.loginfo( "Got message : "+str(self.message))
+						self.pubimu()
+			except:
+				rospy.loginfo("Failed getting message !")
+
+	def pubimu(self):
+		self.p.linear_acceleration.x= (4*9.81*float(self.message[1])/2**16)
+		self.p.linear_acceleration.y=(4*9.81*float(self.message[2])/2**16)
+		self.p.linear_acceleration.z=(4*9.81*float(self.message[3])/2**16)
+		self.p.orientation.x= float(self.message[4])
+		self.p.orientation.y=float(self.message[5])
+		self.p.orientation.z=float(self.message[6])
+		self.pub_imu.publish(self.p)
+
+if __name__=="__main__":
+
+	print "Starting"
+	rospy.init_node("zumo")
+	myZumo=Zumo()
+
+	while not rospy.is_shutdown():
+		myZumo.get_message()
+		sleep(0.001)
+
+	rospy.loginfo("Node terminated")
+	rospy.delete_param("ZUMO_BAUDRATE")
+	rospy.delete_param("ZUMO_PORT")
+	myZumo.ser.close()
+	rospy.loginfo("Connection lost")
